6b755d156a1d2cc109998897d0e83f899466b275
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-11-25 06:50:18 +0000

    Cleanup
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/staffprofiles/trunk@5927 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- code/StaffProfilesPage.php
+++ code/StaffProfilesPage.php
@@ -7,8 +7,6 @@
  */
 class StaffProfilesPage extends Page {
 
-	private static $use_child_pages_rather_than_dataobjects = false;
-
 	private static $icon = "mysite/images/treeicons/StaffProfilesPage";
 
 	private static $allowed_children = array("StaffProfilesOnePerson"); //can also be "none";
@@ -21,27 +19,20 @@ class StaffProfilesPage extends Page {
 
 	function getCMSFields() {
 		$fields = parent::getCMSFields();
-		if(!$this->Config()->get("use_child_pages_rather_than_dataobjects")) {
-			$fields->addFieldToTab(
-				"Root.Profiles",
-				new GridField(
-					"StaffProfiles",
-					"Staff Profiles",
-					StaffProfile::get(),
-					GridFieldConfig_RelationEditor::create()
-				)
-			);
-		}
+		$fields->addFieldToTab(
+			"Root.Profiles",
+			new GridField(
+				"StaffProfiles",
+				"Staff Profiles",
+				StaffProfile::get(),
+				GridFieldConfig_RelationEditor::create()
+			)
+		);
 		return $fields;
 	}
 
 	function StaffProfilesAll() {
-		if(Config::inst()->get("StaffProfilesPage", "use_child_pages_rather_than_dataobjects")) {
-			return StaffProfilesOnePerson::get()->filter(array("ParentID" => $this->ID));
-		}
-		else {
-			return $this->StaffProfiles();
-		}
+		return $this->StaffProfiles();
 	}
 
 
