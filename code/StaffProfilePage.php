7e15c59d3aa914ebc0e6e928f0ede3feb80ff51c
helpfulrobot <chris@silverstripe.com>
2016-01-01 03:52:54 +1300

    Converted to PSR-2

--- code/StaffProfilePage.php
+++ code/StaffProfilePage.php
@@ -5,157 +5,160 @@
  * @author Mattias Lindgren
  * @package staffprofiles
  */
-class StaffProfilePage extends Page {
+class StaffProfilePage extends Page
+{
 
-	/**
-	 * Specify the default parent for staff profiles
-	 *
-	 * @var string
-	 */
-	private static $default_parent = "StaffProfilesHolderPage";	
-	
-	/**
-	 * Staff profiles cannot be in the root
-	 *
-	 * @var bool
-	 */
-	private static $can_be_root = false;
-	
-	/**
-	 * Set custom icon for staff profiles
-	 * 
-	 * @var string
-	 */
-	private static $icon = "blog/images/staff-profiles-page.png";
+    /**
+     * Specify the default parent for staff profiles
+     *
+     * @var string
+     */
+    private static $default_parent = "StaffProfilesHolderPage";
+    
+    /**
+     * Staff profiles cannot be in the root
+     *
+     * @var bool
+     */
+    private static $can_be_root = false;
+    
+    /**
+     * Set custom icon for staff profiles
+     * 
+     * @var string
+     */
+    private static $icon = "blog/images/staff-profiles-page.png";
 
-	/**
-	 * DB fields for staff profile
-	 * 
-	 * @var array 
-	 */
-	private static $db = array(
-		'Position' => 'Varchar(80)',
-		'Email' => 'Varchar(80)',
-		'Phone' => 'Varchar(50)' 
-	);
+    /**
+     * DB fields for staff profile
+     * 
+     * @var array 
+     */
+    private static $db = array(
+        'Position' => 'Varchar(80)',
+        'Email' => 'Varchar(80)',
+        'Phone' => 'Varchar(50)'
+    );
 
-	/**
-	 * Has one relationships
-	 * 
-	 * @var array 
-	 */
-	private static $has_one = array(
-		'Thumbnail' => 'Image'
-	);
-	
-	/**
-	 * Has many many relationship
-	 * 
-	 * @var array 
-	 */
-	private static $many_many = array(
-		'Categories' => 'StaffProfilePage_Category'
-	);
-	
-	/**
-	 * Returns a FieldList with which to create the main editing form.
-	 *
-	 * @return FieldList The fields to be displayed in the CMS.
-	 */
-	public function getCMSFields() {
-		// Get the CMS fields
-		$fields = parent::getCMSFields();
-		
-		// Update the existing fields with labels
-		$nameField = $fields->dataFieldByName("Title");
-		if($nameField) {
-			$nameField->setRightTitle("Staff members name (John Smith etc)");
-		}
-		
-		// Add the fields to the CMS
-		$fields->addFieldToTab("Root.Main", TextField::create("Position",_t("StaffProfilePage.PositionTitle","Position")), "Content");
-		$fields->addFieldToTab("Root.Main", EmailField::create("Email",_t("StaffProfilePage.EmailTitle","Email address")), "Content");
-		$fields->addFieldToTab("Root.Main", TextField::create("Phone",_t("StaffProfilePage.PhoneTitle","Phone")), "Content");
-		$fields->addFieldToTab("Root.Categories", GridField::create("Categories", _t("StaffProfilePage.CategoriesTitle","Categories"), $this->Categories(), StaffProfilePage_Category::getGridFieldConfig()));
-		
-		// Add the thumbnail field
-		$uploadField = UploadField::create("Thumbnail", _t("StaffProfilePage.ThumbnailTitle","Thumbnail"));
-		$uploadField->setFolderName('Staff');
-		$uploadField->getValidator()->setAllowedExtensions(array('jpg', 'jpeg', 'png', 'gif'));
-		$sizeMB = 0.3; // MB
-		$size = $sizeMB * 1024 * 1024; // Bytes
-		$uploadField->getValidator()->setAllowedMaxFileSize($size);		
-		$fields->addFieldToTab("Root.Main", $uploadField, "Content");
-		
-		$this->extend('updateCMSFields', $fields);
+    /**
+     * Has one relationships
+     * 
+     * @var array 
+     */
+    private static $has_one = array(
+        'Thumbnail' => 'Image'
+    );
+    
+    /**
+     * Has many many relationship
+     * 
+     * @var array 
+     */
+    private static $many_many = array(
+        'Categories' => 'StaffProfilePage_Category'
+    );
+    
+    /**
+     * Returns a FieldList with which to create the main editing form.
+     *
+     * @return FieldList The fields to be displayed in the CMS.
+     */
+    public function getCMSFields()
+    {
+        // Get the CMS fields
+        $fields = parent::getCMSFields();
+        
+        // Update the existing fields with labels
+        $nameField = $fields->dataFieldByName("Title");
+        if ($nameField) {
+            $nameField->setRightTitle("Staff members name (John Smith etc)");
+        }
+        
+        // Add the fields to the CMS
+        $fields->addFieldToTab("Root.Main", TextField::create("Position", _t("StaffProfilePage.PositionTitle", "Position")), "Content");
+        $fields->addFieldToTab("Root.Main", EmailField::create("Email", _t("StaffProfilePage.EmailTitle", "Email address")), "Content");
+        $fields->addFieldToTab("Root.Main", TextField::create("Phone", _t("StaffProfilePage.PhoneTitle", "Phone")), "Content");
+        $fields->addFieldToTab("Root.Categories", GridField::create("Categories", _t("StaffProfilePage.CategoriesTitle", "Categories"), $this->Categories(), StaffProfilePage_Category::getGridFieldConfig()));
+        
+        // Add the thumbnail field
+        $uploadField = UploadField::create("Thumbnail", _t("StaffProfilePage.ThumbnailTitle", "Thumbnail"));
+        $uploadField->setFolderName('Staff');
+        $uploadField->getValidator()->setAllowedExtensions(array('jpg', 'jpeg', 'png', 'gif'));
+        $sizeMB = 0.3; // MB
+        $size = $sizeMB * 1024 * 1024; // Bytes
+        $uploadField->getValidator()->setAllowedMaxFileSize($size);
+        $fields->addFieldToTab("Root.Main", $uploadField, "Content");
+        
+        $this->extend('updateCMSFields', $fields);
 
-		return $fields;
-	}
-	
-	/**
-	 * Create a unique email link that is connected with the Contact Form
-	 * 
-	 * @return string get the link to the contact page
-	 */
-	public function EmailLink() {
-		if(!empty($this->Email)) {
-			$page = ContactPage::get()->first();
-			if($page) {
-				return $page->Link() . "staff/{$this->ID}";
-			}
-		}
-		return false;
-	}
+        return $fields;
+    }
+    
+    /**
+     * Create a unique email link that is connected with the Contact Form
+     * 
+     * @return string get the link to the contact page
+     */
+    public function EmailLink()
+    {
+        if (!empty($this->Email)) {
+            $page = ContactPage::get()->first();
+            if ($page) {
+                return $page->Link() . "staff/{$this->ID}";
+            }
+        }
+        return false;
+    }
 }
 
 /**
  * Staff profile page controller
  */
-class StaffProfilePage_Controller extends Page_Controller {
-
+class StaffProfilePage_Controller extends Page_Controller
+{
 }
 
 /**
  * Staff profile page category
  */
-class StaffProfilePage_Category extends DataObject {
-	/**
-	 * DB fields
-	 * 
-	 * @var array
-	 */
-	private static $db = array(
-		'Title' => 'Varchar(80)'
-	);
-	
-	/**
-	 * Has many many relationships reverse
-	 * 
-	 * @var array
-	 */
-	private static $belongs_many_many = array(
-		'StaffProfilePages' => 'StaffProfilePage'
-	);
-	
-	/**
-	 * Get the configuration for the Gridfield component
-	 * 
-	 * @return GridFieldConfig
-	 */
-	public static function getGridFieldConfig() {
-		$config = GridFieldConfig::create();
-		$config->addComponent(new GridFieldButtonRow("before"));
-		$config->addComponent(new GridFieldAddNewButton("buttons-before-left"));
-		$config->addComponent(new GridFieldAddExistingAutocompleter("buttons-before-left"));
-		$config->addComponent(new GridFieldToolbarHeader());
-		$config->addComponent(new GridFieldDataColumns());
-		$config->addComponent(new GridFieldEditButton());
-		$config->addComponent(new GridFieldDeleteAction(true));
-		$config->addComponent(new GridFieldPaginator(30));
-		$config->addComponents(new GridFieldDetailForm());
-		// $config->addComponent(new GridFieldSortableRows("Sort"));
-		return $config;
-	}
+class StaffProfilePage_Category extends DataObject
+{
+    /**
+     * DB fields
+     * 
+     * @var array
+     */
+    private static $db = array(
+        'Title' => 'Varchar(80)'
+    );
+    
+    /**
+     * Has many many relationships reverse
+     * 
+     * @var array
+     */
+    private static $belongs_many_many = array(
+        'StaffProfilePages' => 'StaffProfilePage'
+    );
+    
+    /**
+     * Get the configuration for the Gridfield component
+     * 
+     * @return GridFieldConfig
+     */
+    public static function getGridFieldConfig()
+    {
+        $config = GridFieldConfig::create();
+        $config->addComponent(new GridFieldButtonRow("before"));
+        $config->addComponent(new GridFieldAddNewButton("buttons-before-left"));
+        $config->addComponent(new GridFieldAddExistingAutocompleter("buttons-before-left"));
+        $config->addComponent(new GridFieldToolbarHeader());
+        $config->addComponent(new GridFieldDataColumns());
+        $config->addComponent(new GridFieldEditButton());
+        $config->addComponent(new GridFieldDeleteAction(true));
+        $config->addComponent(new GridFieldPaginator(30));
+        $config->addComponents(new GridFieldDetailForm());
+        // $config->addComponent(new GridFieldSortableRows("Sort"));
+        return $config;
+    }
 }
-
-
--- code/StaffProfilesHolderPage.php
+++ code/StaffProfilesHolderPage.php
@@ -5,116 +5,122 @@
  * @author Mattias Lindgren
  * @package staffprofiles
  */
-class StaffProfilesHolderPage extends Page {
-	/**
-	 * Specify the allowed child pages
-	 * 
-	 * @var array 
-	 */
-	private static $allowed_children = array(
-		'StaffProfilePage'
-	);
-	
-	/**
-	 * Set custom icon for staff profiles
-	 * 
-	 * @var string
-	 */
-	private static $icon = "blog/images/staff-profiles-holder-page.png";
+class StaffProfilesHolderPage extends Page
+{
+    /**
+     * Specify the allowed child pages
+     * 
+     * @var array 
+     */
+    private static $allowed_children = array(
+        'StaffProfilePage'
+    );
+    
+    /**
+     * Set custom icon for staff profiles
+     * 
+     * @var string
+     */
+    private static $icon = "blog/images/staff-profiles-holder-page.png";
 }
 
 /**
  * Staff profiles holder page controller
  */
-class StaffProfilesHolderPage_Controller extends Page_Controller {
-	
-	/**
-	 * Current category
-	 * 
-	 * @var int 
-	 */
-	protected $categoryID = 0;
-	
-	/**
-	 * Enable category request handler
-	 * 
-	 * @var array 
-	 */
-	private static $allowed_actions = array (
-		"category"
-	);	
-	
-	/**
-	 * Handle category requests
-	 */
-	public function category() {
-		// Get the requested category
-		$category = SeoFriendlyDataObject::get_by_url_segement(
-			"StaffProfilePage_Category",
-			Convert::raw2sql($this->request->param("ID"))
-		);
-		
-		// Set the current category
-		if($category){
-			$this->categoryID = $category->ID;
-		}
+class StaffProfilesHolderPage_Controller extends Page_Controller
+{
+    
+    /**
+     * Current category
+     * 
+     * @var int 
+     */
+    protected $categoryID = 0;
+    
+    /**
+     * Enable category request handler
+     * 
+     * @var array 
+     */
+    private static $allowed_actions = array(
+        "category"
+    );
+    
+    /**
+     * Handle category requests
+     */
+    public function category()
+    {
+        // Get the requested category
+        $category = SeoFriendlyDataObject::get_by_url_segement(
+            "StaffProfilePage_Category",
+            Convert::raw2sql($this->request->param("ID"))
+        );
+        
+        // Set the current category
+        if ($category) {
+            $this->categoryID = $category->ID;
+        }
 
-		return $this;
-	}
-	
-	/**
-	 * Get a list of staff profiles based on selected category
-	 * 
-	 * @return SS_List StaffProfilePage
-	 */
-	public function StaffProfiles(){
-		$results = StaffProfilePage::get()->filter(array(
-				'ParentID' => $this->ID
-			));
+        return $this;
+    }
+    
+    /**
+     * Get a list of staff profiles based on selected category
+     * 
+     * @return SS_List StaffProfilePage
+     */
+    public function StaffProfiles()
+    {
+        $results = StaffProfilePage::get()->filter(array(
+                'ParentID' => $this->ID
+            ));
 
-		if($this->categoryID > 0) {
-			$results = $results
-				->innerJoin("StaffProfilePage_Categories", "`StaffProfilePage_Categories`.`StaffProfilePageID` = `StaffProfilePage`.`ID`")
-				->where("`StaffProfilePage_Categories`.`StaffProfilePage_CategoryID` = {$this->categoryID}");
-		}
-		
-		return $results;
-	}
-	
-	/**
-	 * Get the current category ID
-	 * 
-	 * @return int
-	 */
-	public function CurrentCategoryID(){
-		return $this->categoryID;
-	}
-	
-	/**
-	 * Get a list of categories related to this 
-	 * staff profiles holder page.
-	 * 
-	 * @return SS_List StaffProfilePage_Category
-	 */
-	public function Categories(){
-		// Create the output list
-		$output = new ArrayList();
-		
-		// Get all the categories
-		$categories = StaffProfilePage_Category::get();
-		
-		// Create the all category
-		$allCategory = StaffProfilePage_Category::create();
-		$allCategory->Title = "All";
-		$allCategory->Selected = ($this->categoryID === 0) ? true : false;
-		$output->add($allCategory);
-		
-		// Add all the categories
-		foreach($categories as $category){
-			$category->Selected = ($this->categoryID === $category->ID) ? true : false;
-			$output->add($category);
-		}
+        if ($this->categoryID > 0) {
+            $results = $results
+                ->innerJoin("StaffProfilePage_Categories", "`StaffProfilePage_Categories`.`StaffProfilePageID` = `StaffProfilePage`.`ID`")
+                ->where("`StaffProfilePage_Categories`.`StaffProfilePage_CategoryID` = {$this->categoryID}");
+        }
+        
+        return $results;
+    }
+    
+    /**
+     * Get the current category ID
+     * 
+     * @return int
+     */
+    public function CurrentCategoryID()
+    {
+        return $this->categoryID;
+    }
+    
+    /**
+     * Get a list of categories related to this 
+     * staff profiles holder page.
+     * 
+     * @return SS_List StaffProfilePage_Category
+     */
+    public function Categories()
+    {
+        // Create the output list
+        $output = new ArrayList();
+        
+        // Get all the categories
+        $categories = StaffProfilePage_Category::get();
+        
+        // Create the all category
+        $allCategory = StaffProfilePage_Category::create();
+        $allCategory->Title = "All";
+        $allCategory->Selected = ($this->categoryID === 0) ? true : false;
+        $output->add($allCategory);
+        
+        // Add all the categories
+        foreach ($categories as $category) {
+            $category->Selected = ($this->categoryID === $category->ID) ? true : false;
+            $output->add($category);
+        }
 
-		return $output;
-	}
+        return $output;
+    }
 }
