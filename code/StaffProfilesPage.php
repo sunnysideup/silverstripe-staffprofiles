c33d50a8a8da3804e0f9aaa5871eadc16fe41923
sunnysideup <sunnysideupdevs@gmail.com>
2016-11-29 13:56:01 +1300

    PATCH: module clean-up

--- code/StaffProfilesPage.php
+++ code/StaffProfilesPage.php
@@ -5,69 +5,66 @@
  *@description: displays a list of staff profiles
  *
  */
-class StaffProfilesPage extends Page {
+class StaffProfilesPage extends Page
+{
+    private static $icon = "mysite/images/treeicons/StaffProfilesPage";
 
-	private static $icon = "mysite/images/treeicons/StaffProfilesPage";
+    private static $allowed_children = array("StaffProfilesOnePerson"); //can also be "none";
 
-	private static $allowed_children = array("StaffProfilesOnePerson"); //can also be "none";
+    private static $default_child = "StaffProfilesOnePerson";
 
-	private static $default_child = "StaffProfilesOnePerson";
+    private static $db = array(
+        "DefaultEmail" => "Varchar(255)",
+        "SubjectLine" => "Varchar(255)"
+    );
 
-	private static $db = array(
-		"DefaultEmail" => "Varchar(255)",
-		"SubjectLine" => "Varchar(255)"
-	);
+    private static $has_many = array(
+        "StaffProfiles" => "StaffProfile"
+    );
 
-	private static $has_many = array(
-		"StaffProfiles" => "StaffProfile"
-	);
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->addFieldToTab("Root.Profiles", $defaultEmailTextField = new TextField("DefaultEmail"));
+        $defaultEmailTextField->setRightTitle(
+            _t(
+                "StaffProfilesPage.DEFAULT_EMAIL_EXPLANATION",
+                "This is the default email that will be used if a staff member does not have a unique email"
+            )
+        );
+        $fields->addFieldToTab("Root.Profiles", $subjectLineTextField = new TextField("SubjectLine"));
+        $subjectLineTextField->setRightTitle(
+            _t(
+                "StaffProfilesPage.SUBJECT_LINE_EXPLANATION",
+                "Subject line for email, you can use [". implode("], [", array_keys(Config::inst()->get("StaffProfile", "subject_place_holders"))). "]" . " as placeholders"
+            )
+        );
+        $fields->addFieldToTab(
+            "Root.Profiles",
+            new GridField(
+                "StaffProfiles",
+                "Staff Profiles",
+                StaffProfile::get(),
+                $config = GridFieldConfig_RelationEditor::create()
+            )
+        );
+        if (class_exists("GridFieldSortableRows")) {
+            $config->addComponent(new GridFieldSortableRows('Sort'));
+        }
+        return $fields;
+    }
 
-	function getCMSFields() {
-		$fields = parent::getCMSFields();
-		$fields->addFieldToTab("Root.Profiles", $defaultEmailTextField = new TextField("DefaultEmail"));
-		$defaultEmailTextField->setRightTitle(
-			_t(
-				"StaffProfilesPage.DEFAULT_EMAIL_EXPLANATION",
-				"This is the default email that will be used if a staff member does not have a unique email"
-			)
-		);
-		$fields->addFieldToTab("Root.Profiles", $subjectLineTextField = new TextField("SubjectLine"));
-		$subjectLineTextField->setRightTitle(
-			_t(
-				"StaffProfilesPage.SUBJECT_LINE_EXPLANATION",
-				"Subject line for email, you can use [". implode("], [", array_keys(Config::inst()->get("StaffProfile", "subject_place_holders"))). "]" . " as placeholders"
-			)
-		);
-		$fields->addFieldToTab(
-			"Root.Profiles",
-			new GridField(
-				"StaffProfiles",
-				"Staff Profiles",
-				StaffProfile::get(),
-				$config = GridFieldConfig_RelationEditor::create()
-			)
-		);
-		if(class_exists("GridFieldSortableRows")) {
-			$config->addComponent(new GridFieldSortableRows('Sort'));
-		}
-		return $fields;
-	}
-
-	function StaffProfilesAll() {
-		return $this->StaffProfiles();
-	}
-
-
-
+    public function StaffProfilesAll()
+    {
+        return $this->StaffProfiles();
+    }
 }
 
-class StaffProfilesPage_Controller extends Page_Controller {
-
-	function init() {
-		parent::init();
-		Requirements::themedCSS("StaffProfilesPage", "staffprofiles");
-	}
-
-
+class StaffProfilesPage_Controller extends Page_Controller
+{
+    public function init()
+    {
+        parent::init();
+        Requirements::themedCSS("StaffProfilesPage", "staffprofiles");
+    }
 }
-
--- code/model/StaffProfile.php
+++ code/model/StaffProfile.php
@@ -6,216 +6,216 @@
  *
  **/
 
-class StaffProfile extends DataObject {
+class StaffProfile extends DataObject
+{
+    private static $db = array(
+        "Name" => "Varchar(255)",
+        "Position" => "Varchar(255)",
+        "Description" => "Text",
+        "Email" => "Varchar(255)",
+        "SubjectLine" => "Varchar(255)",
+        "Sort" => "Int"
+    );
 
-	private static $db = array(
-		"Name" => "Varchar(255)",
-		"Position" => "Varchar(255)",
-		"Description" => "Text",
-		"Email" => "Varchar(255)",
-		"SubjectLine" => "Varchar(255)",
-		"Sort" => "Int"
-	);
+    private static $has_one = array(
+        "ProfilePicture" => "Image",
+        "Parent" => "StaffProfilesPage"
+    );
 
-	private static $has_one = array(
-		"ProfilePicture" => "Image",
-		"Parent" => "StaffProfilesPage"
-	);
+    //database related settings
+    private static $indexes = array(
+        "Sort" => true
+    );
 
-	//database related settings
-	private static $indexes = array(
-		"Sort" => true
-	);
+    //formatting
+    private static $searchable_fields = array("Name" => "PartialMatchFilter");
 
-	//formatting
-	private static $searchable_fields = array("Name" => "PartialMatchFilter");
+    private static $field_labels = array(
+        "SortNumber" => "Sort Index Number for Sorting (lower numbers first)",
+        "Subjectline" => "Optional Subject Line"
+    );
 
-	private static $field_labels = array(
-		"SortNumber" => "Sort Index Number for Sorting (lower numbers first)",
-		"Subjectline" => "Optional Subject Line"
-	);
+    private static $summary_fields = array("Name" => "Name", "Email" => "Email", "Title" => "Title");
 
-	private static $summary_fields = array("Name" => "Name", "Email" => "Email", "Title" => "Title");
+    private static $singular_name = "Staff Profile";
 
-	private static $singular_name = "Staff Profile";
+    private static $plural_name = "Staff Profiles";
 
-	private static $plural_name = "Staff Profiles";
+    private static $default_sort = "Sort ASC, Name ASC";
 
-	private static $default_sort = "Sort ASC, Name ASC";
+    private static $defaults = array(
+        "Sort" => 100
+    );
 
-	private static $defaults = array(
-		"Sort" => 100
-	);
+    /**
+     * replacement placeholders
+     * [xxx] => yyy
+     * where xxx is the string the CMS user types
+     * and yyy the replacement field / relation.
+     *
+     * @var array
+     */
+    private static $subject_place_holders = array(
+        "Name" => "Name",
+        "Email" => "Email",
+        "Position" => "Position",
+        "PageTitle" => "Parent.Title",
+        "PageLink" => "Parent.Link"
+    );
 
-	/**
-	 * replacement placeholders
-	 * [xxx] => yyy
-	 * where xxx is the string the CMS user types
-	 * and yyy the replacement field / relation.
-	 *
-	 * @var array
-	 */
-	private static $subject_place_holders = array(
-		"Name" => "Name",
-		"Email" => "Email",
-		"Position" => "Position",
-		"PageTitle" => "Parent.Title",
-		"PageLink" => "Parent.Link"
-	);
+    public function populateDefaults()
+    {
+        $this->Sort = 100;
+        parent::populateDefaults();
+    }
 
-	public function populateDefaults() {
-		$this->Sort = 100;
-		parent::populateDefaults();
-	}
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $field = $fields->dataFieldByName("SubjectLine");
+        $field->setRightTitle(
+            _t("StaffProfile.PLACEHOLDER_EXPLANATION", "you can use the following placeholders")
+            . ": ["
+            . implode("], [", array_keys($this->Config()->get("subject_place_holders")))
+            . "]"
+        );
+        if (class_exists("DataObjectSorterController") && $this->hasExtension("DataObjectSorterController")) {
+            $fields->addFieldToTab("Root.Sort", new LiteralField("InvitationToSort", $this->dataObjectSorterPopupLink()));
+            $fields->removeFieldFromTab("Root.Main", "Sort");
+        }
+        return $fields;
+    }
 
-	public function getCMSFields(){
-		$fields = parent::getCMSFields();
-		$field = $fields->dataFieldByName("SubjectLine");
-		$field->setRightTitle(
-			_t("StaffProfile.PLACEHOLDER_EXPLANATION", "you can use the following placeholders")
-			. ": ["
-			. implode("], [", array_keys($this->Config()->get("subject_place_holders")))
-			. "]"
-		);
-		if(class_exists("DataObjectSorterController") && $this->hasExtension("DataObjectSorterController")) {
-			$fields->addFieldToTab("Root.Sort", new LiteralField("InvitationToSort", $this->dataObjectSorterPopupLink()));
-			$fields->removeFieldFromTab("Root.Main", "Sort");
-		}
-		return $fields;
-	}
+    public function fieldLabels($includeRelations = true)
+    {
+        $labels = parent::fieldLabels($includeRelations);
+        return $labels;
+    }
 
-	public function fieldLabels($includeRelations = true) {
-		$labels = parent::fieldLabels($includeRelations);
-		return $labels;
-	}
+    /**
+     * Obscure all email links in StringField.
+     * Matches mailto:user@example.com as well as user@example.com
+     *
+     * @return string | Null
+     */
+    public function EncodedEmailLink()
+    {
+        if ($email = $this->getBestEmail()) {
+            $obj = $this->retrieveEmailObject();
+            if ($obj) {
+                return $obj->MailTo;
+            } else {
+                return "mailto:".$email;
+            }
+        }
+    }
 
-	/**
-	 * Obscure all email links in StringField.
-	 * Matches mailto:user@example.com as well as user@example.com
-	 *
-	 * @return string | Null
-	 */
-	public function EncodedEmailLink() {
-		if($email = $this->getBestEmail()) {
-			$obj = $this->retrieveEmailObject();
-			if($obj) {
-				return $obj->MailTo;
-			}
-			else {
-				return "mailto:".$email;
-			}
-		}
-	}
+    /**
+     * Obscure all email links in StringField.
+     * Matches mailto:user@example.com as well as user@example.com
+     *
+     * @return string
+     */
+    public function EncodedEmailText()
+    {
+        if ($email = $this->getBestEmail()) {
+            $obj = $this->retrieveEmailObject();
+            if ($obj) {
+                return $obj->Text;
+            } else {
+                return $email;
+            }
+        }
+    }
 
-	/**
-	 * Obscure all email links in StringField.
-	 * Matches mailto:user@example.com as well as user@example.com
-	 *
-	 * @return string
-	 */
-	public function EncodedEmailText() {
-		if($email = $this->getBestEmail()) {
-			$obj = $this->retrieveEmailObject();
-			if($obj) {
-				return $obj->Text;
-			}
-			else {
-				return $email;
-			}
-		}
-	}
+    public function onBeforeWrite()
+    {
+        parent::onBeforeWrite();
+        if (!$this->Sort) {
+            $this->Sort = 100;
+        }
+        if (!$this->ParentID) {
+            $page = StaffProfilesPage::get()->First();
+            $this->ParentID = $page->ID;
+        }
+    }
 
-	function onBeforeWrite() {
-		parent::onBeforeWrite();
-		if(!$this->Sort) {
-			$this->Sort = 100;
-		}
-		if(!$this->ParentID) {
-			$page = StaffProfilesPage::get()->First();
-			$this->ParentID = $page->ID;
-		}
-	}
-
-	/**
-	 * puts together a subject line with replacements
-	 *
-	 * @return String
-	 */
-	protected function SubjectLineCreator(){
-		if($this->SubjectLine) {
-			$str = $this->SubjectLine;
-		}
-		else {
-			$str = $this->Parent()->SubjectLine;
-		}
-		if(!$str) {
-			$str =  "Enquiry from [PageLink] for [Name]";
-		}
-		$replace = $this->Config()->get("subject_place_holders");
-		foreach($replace as $findKey => $replaceField) {
-			if(strpos($str, $findKey) !== null) {
-				if(strpos($replaceField, ".")) {
-					$replaceFieldParts = explode(".", $replaceField);
-					$method1 = $replaceFieldParts[0];
-					$method2 = $replaceFieldParts[1];
-					$relationalObject = $this->$method1();
-					if($relationalObject) {
-						if($relationalObject->hasMethod($method2)) {
-							$replaceValue = $relationalObject->$method2();
-						}
-						elseif($relationalObject->hasMethod("get".$method2)) {
-							$method2 = "get".$method2;
-							$replaceValue = $relationalObject->$method2();
-						}
-					}
-				}
-				else {
-					$replaceValue = $this->$replaceField;
-				}
-				$str = str_ireplace("[".$findKey."]", $replaceValue, $str);
-			}
-		}
-		return $str;
-	}
-
-
-	/**
-	 * @var EmailObject
-	 */
-	protected $emailObject = null;
-
-	/**
-	 *
-	 * @return EmailObject | NULL
-	 */
-	protected function retrieveEmailObject(){
-		if(!$this->emailObject) {
-			if(class_exists("HideMailto")) {
-				if($email = $this->getBestEmail()) {
-					$this->emailObject = HideMailto::convert_email($email, $this->SubjectLineCreator());
-				}
-			}
-			else {
-				user_error("This module requires Sunnysideup/hidemailto, but it can function without it", E_USER_NOTICE);
-			}
-		}
-		return $this->emailObject;
-	}
-
-	/**
-	 * finds the best email available.
-	 *
-	 * @return String
-	 */
-	protected function getBestEmail(){
-		if($this->Email) {
-			$email = $this->Email;
-		}
-		else {
-			$email = $this->Parent()->DefaultEmail;
-		}
-		return $email;
-	}
+    /**
+     * puts together a subject line with replacements
+     *
+     * @return String
+     */
+    protected function SubjectLineCreator()
+    {
+        if ($this->SubjectLine) {
+            $str = $this->SubjectLine;
+        } else {
+            $str = $this->Parent()->SubjectLine;
+        }
+        if (!$str) {
+            $str =  "Enquiry from [PageLink] for [Name]";
+        }
+        $replace = $this->Config()->get("subject_place_holders");
+        foreach ($replace as $findKey => $replaceField) {
+            if (strpos($str, $findKey) !== null) {
+                if (strpos($replaceField, ".")) {
+                    $replaceFieldParts = explode(".", $replaceField);
+                    $method1 = $replaceFieldParts[0];
+                    $method2 = $replaceFieldParts[1];
+                    $relationalObject = $this->$method1();
+                    if ($relationalObject) {
+                        if ($relationalObject->hasMethod($method2)) {
+                            $replaceValue = $relationalObject->$method2();
+                        } elseif ($relationalObject->hasMethod("get".$method2)) {
+                            $method2 = "get".$method2;
+                            $replaceValue = $relationalObject->$method2();
+                        }
+                    }
+                } else {
+                    $replaceValue = $this->$replaceField;
+                }
+                $str = str_ireplace("[".$findKey."]", $replaceValue, $str);
+            }
+        }
+        return $str;
+    }
 
 
+    /**
+     * @var EmailObject
+     */
+    protected $emailObject = null;
+
+    /**
+     *
+     * @return EmailObject | NULL
+     */
+    protected function retrieveEmailObject()
+    {
+        if (!$this->emailObject) {
+            if (class_exists("HideMailto")) {
+                if ($email = $this->getBestEmail()) {
+                    $this->emailObject = HideMailto::convert_email($email, $this->SubjectLineCreator());
+                }
+            } else {
+                user_error("This module requires Sunnysideup/hidemailto, but it can function without it", E_USER_NOTICE);
+            }
+        }
+        return $this->emailObject;
+    }
+
+    /**
+     * finds the best email available.
+     *
+     * @return String
+     */
+    protected function getBestEmail()
+    {
+        if ($this->Email) {
+            $email = $this->Email;
+        } else {
+            $email = $this->Parent()->DefaultEmail;
+        }
+        return $email;
+    }
 }
